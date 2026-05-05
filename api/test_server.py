e93270e77e70b1519efdbf4e8625dea7751330b0
epicgdog <91296067+epicgdog@users.noreply.github.com>
2026-05-04 18:14:04 -0700

    added ssh tunneling

---
+++ api/test_server.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+import subprocess
+import os
+app = FastAPI()
+
+
+@app.get("/")
+async def execute_curl():
+    """
+    Executes the predefined curl command and returns the output.
+    """
+    try:
+        # Run the curl command
+        result = subprocess.run(
+            ["curl", "-X", "GET", f"{os.getenv("ESP_IP")}/json/devices/"],
+            capture_output=True,  # Captures stdout and stderr
+            text=True,            # Returns strings instead of bytes
+            check=True            # Raises CalledProcessError if the exit code is non-zero
+        )
+        
+        # Return the output of the curl command
+        return {
+            "status": "success",
+            "output": result.stdout.strip()
+        }
+    except Exception as e:
+        # Handles any other unexpected Python errors
+        raise HTTPException(
+            status_code=500, 
+            detail={"error": "An unexpected error occurred", "message": str(e)}
+        )
--- api/tun.sh
+++ api/tun.sh
@@ -1,47 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-CORE_V4_IP=$(cat /app/config/config.json | jq -r ".CORE_V4_IP") 
+set -x
 
-DOCKER_CONTAINER_KNOWN_HOSTS=/app/ssh/known_hosts
+CLARK_IP=$(cat /app/config/config.json |  jq -r ".CLARK_IP")
 
-DOCKER_CONTAINER_SSH_KEYS=/app/ssh/id_rsa
+SSH_KEY=/app/ssh_key
+SSH_KNOWN_HOSTS=/app/known_hosts
 
-CORE_V4_PORT=10000 
+CLARK_PORT=5055
+BLE_PORT=5055
 
-LEDSIGN_PORT=10000
-
-CORE_V4_HOST=sce@${CORE_V4_IP}
+CLARK_HOST=sce@${CLARK_IP}
 
 open_ssh_tunnel () {
-    echo "running command"
-    echo "ssh \
-    -o UserKnownHostsFile=${DOCKER_CONTAINER_KNOWN_HOSTS} \
-    -o StrictHostKeyChecking=no \
-    -i ${DOCKER_CONTAINER_SSH_KEYS} \
-    -f -g -N -R 0.0.0.0:${CORE_V4_PORT}:localhost:${LEDSIGN_PORT} ${CORE_V4_HOST}"
     ssh \
-    -o UserKnownHostsFile=${DOCKER_CONTAINER_KNOWN_HOSTS} \
+    -o UserKnownHostsFile=${SSH_KNOWN_HOSTS} \
     -o StrictHostKeyChecking=no \
-    -i ${DOCKER_CONTAINER_SSH_KEYS} \
-    -f -g -N -R 0.0.0.0:${CORE_V4_PORT}:localhost:${LEDSIGN_PORT} ${CORE_V4_HOST}
+    -i ${SSH_KEY} \
+    -f -g -N -R 0.0.0.0:${CLARK_PORT}:localhost:${BLE_PORT} ${CLARK_HOST}
 }
 
-ls /app
-ls /app/ssh
+chmod 600 ${SSH_KEY}
 
-chmod 600 ${DOCKER_CONTAINER_SSH_KEYS}
+# setup the esp_ip in the env
+export ESP_IP=$(cat /app/config/config.json |  jq -r ".ESP_IP")
 
 open_ssh_tunnel
-
-# if no value is sent along with the invocation of the script,
-# run the server. otherwise just open the ssh tunnel. i.e.
-#
-# to open the tunnel and start the server:
-# $ ./tun.sh 
-#
-# to only open the tunnel:
-# $ ./tun.sh tunnel-only
-if [ -z "$1" ]
-then
-    python /app/api/server.py
-fi
+exec uvicorn api.test_server:app --host 0.0.0.0 --port 5055
